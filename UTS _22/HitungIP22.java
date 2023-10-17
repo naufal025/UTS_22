2933323ca601832ca139d91fb468106b08b525d2
Naufal Gajendra Ardiputra <53498520+naufal025@users.noreply.github.com>
2023-10-17 14:59:12 +0700

    Update HitungIP22.java

--- UTS _22/HitungIP22.java
+++ UTS _22/HitungIP22.java
@@ -1,72 +1,85 @@
 import java.util.Scanner;
-public class HitungIP22{
+
+public class HitungIP22 {
     public static void main(String[] args) {
-        Scanner input22 = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
 
-        String nama,matkul,huruf; 
-        int sks,jmlmatkul = 0,remidi = 0,jmlhSks = 0;
-        char ulang = 'N';
-        double jmlhIp = 0;
+        String namaMahasiswa, namaMataKuliah, nilai, konf="y";
+        double IP=0.0, totalIPSKS = 0, indeksNilai = 0.0;
+        int jumlahMataKuliah=0, sks, totalSKS = 0, jumlahRemidi = 0;
 
-        System.out.println("=======Program Hitung IP=======");
-        System.out.print("Masukkan nama : ");
-        nama = input22.nextLine();
+        System.out.print("Masukkan nama mahasiswa: ");
+        namaMahasiswa = input.nextLine();
+        do {
 
-        do{
-        System.out.print("Nama Mata Kuliah : ");
-        matkul = input22.nextLine();
-        jmlmatkul++;
-        System.out.print("Nilai (A,B+,B,C+,C,D,E): ");
-        huruf = input22.next();
-        //konversi nilai
-        double bobot = 0.0;
-        switch (huruf) {
-            case "A":
-                bobot = 4.0;
-                break;
-            case "B+":
-                bobot = 3.5;
-                break;
-            case "B":
-                bobot = 3.0;
-                break;
-            case "C+":
-                bobot = 2.5;
-                break;
-            case "C":
-                bobot = 2.0;
-                break;
-            case "D":
-                bobot = 1.0;
-                remidi++;
-                break;
-            case "E":
-                bobot = 0.0;
-                remidi++;
-                break;
-            default:
-                System.out.println("Nilai tidak valid untuk mata kuliah ini.");
-                continue; // Skip iterasi selanjutnya
+            // System.out.print("Masukkan jumlah mata kuliah: ");
+            // jumlahMataKuliah = input.nextInt();
+
+            // System.out.println("Mata Kuliah ke-" + i);
+            // input.nextLine(); // Membersihkan buffer
+            System.out.print("Nama Mata Kuliah: ");
+            namaMataKuliah = input.nextLine();
+
+            System.out.print("Nilai (A, B+, B, C+, C, D, E): ");
+            nilai = input.nextLine();
+
+            switch (nilai) {
+                case "A":
+                    indeksNilai = 4.0;
+                    break;
+                case "B+":
+                    indeksNilai = 3.5;
+                    break;
+                case "B":
+                    indeksNilai = 3.0;
+                    break;
+                case "C+":
+                    indeksNilai = 2.5;
+                    break;
+                case "C":
+                    indeksNilai = 2.0;
+                    break;
+                case "D":
+                    indeksNilai = 1.0;
+                    jumlahRemidi++;
+                    break;
+                case "E":
+                    indeksNilai = 0.0;
+                    jumlahRemidi++;
+                    break;
+                default:
+                    System.out.println("Nilai tidak valid. Nilai diabaikan.");
+                    continue; // Skip this iteration and move to the next
+            }
+            System.out.print("SKS: ");
+            sks = input.nextInt();
+            
+            totalIPSKS += indeksNilai * sks;
+            totalSKS += sks;
+            jumlahMataKuliah++;
+
+            input.nextLine();
+
+            System.out.print("Apakah lanjut input nilai (Y/N): ");
+            konf = input.nextLine().toUpperCase();
+        }while (konf.equalsIgnoreCase("y"));
+            
+            // switch (konf) {
+            //     case "n":
+                    
+            //         break;
+            // }
+        if (totalSKS>0) {
+            IP = totalIPSKS / totalSKS;
         }
 
-        System.out.print("Masukkan jumlah SKS : ");
-        sks = input22.nextInt();
-        
-        jmlhIp += bobot * sks;
-        jmlhSks += sks;
+        System.out.println("===== Hasil Perhitungan IP =====");
+        System.out.println("Nama Mahasiswa: " + namaMahasiswa);
+        System.out.println("Indeks Prestasi (IP): " + IP);
+        System.out.println("Total Mata Kuliah: " + jumlahMataKuliah);
+        System.out.println("Total SKS: " + totalSKS);
+        System.out.println("Jumlah Mata Kuliah yang harus perbaikan/remidi: " + jumlahRemidi);
 
-        System.out.print("Apakah Lanjut Input Nilai (Y/N)? ");
-        ulang = input22.next().charAt(0);
-
-     } while (ulang != 'N');
-
-        double totalNilai = jmlhIp / jmlhSks;
-
-        System.out.println("------------Hasil Perhitungan IP------------");
-        System.out.println("Nama                    : " +nama);
-        System.out.println("Indeks Prestasi (IP)    : " +totalNilai) ;
-        System.out.println("Jumlah mata kuliah      : " +jmlmatkul);
-        System.out.println("Jumlah SKS              : " +jmlhSks);
-        System.out.println("Jumlah MK yang harus perbaikan / remidi : " +remidi);
-}
+        input.close();
+    }
 }
